05c454ccdb036421ffc54a1b88813a72215ea162
Vinicius P. M. Miguel <viniciusp070204@gmail.com>
2026-05-24 17:09:20 -0300

    fiz path no install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ docker-compose up -d sender
 
 PORT=$(docker port mqtt-sender-${HOSTNAME}-${VERSION_DOCKER} 4815/tcp | cut -d: -f2)
 
-printf '\nSENDER_PORT=%s\n' "$PORT" >> ./sender/.env
+printf '\nSENDER_PORT=%s\n' "$PORT" >> ./simulator/.env
 printf '\nexport SENDER_PORT=%s\n' "$PORT" >> .env
 
 docker-compose up -d simulator
