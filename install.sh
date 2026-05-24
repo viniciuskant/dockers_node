f3bd0041185a9968f60d3fb85c6121b5b1f9babe
Vinicius P. M. Miguel <viniciusp070204@gmail.com>
2026-05-24 16:10:47 -0300

    arrumando export do install

--- install.sh
+++ install.sh
@@ -79,8 +79,8 @@ docker-compose up -d sender
 
 PORT=$(docker port mqtt-sender-${HOSTNAME}-${VERSION_DOCKER} 4815/tcp | cut -d: -f2)
 
-echo "SENDER_PORT=$PORT" >> ./sender/.env
-echo "export SENDER_PORT=$PORT" >> .env
+echo "$(\n)SENDER_PORT=$PORT" >> ./sender/.env
+echo "$(\n)export SENDER_PORT=$PORT" >> .env
 
 docker-compose up -d simulator
 
