0e91c86843a3e112f04573bc294c6a4b25da11aa
Vinicius P. M. Miguel <viniciusp070204@gmail.com>
2026-06-08 21:40:35 -0300

    test

--- esp/build_esp.sh
+++ esp/build_esp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-touch a
-mkdir build
 
-arduino-cli compile --fqbn esp32:esp32:esp32 --build-path ./build
-sleep 1
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mv build/esp.ino.bin .
-rm -rf build
+mkdir -p "$SCRIPT_DIR/build"
+
+arduino-cli compile  --fqbn esp32:esp32:esp32  --build-path "$SCRIPT_DIR/build"
+
+mv "$SCRIPT_DIR/build/esp.ino.bin" "$SCRIPT_DIR/"
+rm -rf "$SCRIPT_DIR/build"
